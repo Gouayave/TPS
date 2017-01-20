b83f8d96fe93c558ad52820ff1b7e65118dc8b84
Guillaume GENCE <guillaume.gence@gmail.com>
2017-01-20 16:39:44 +0100

    :)

--- index.js
+++ index.js
@@ -10,19 +10,46 @@ var width = 960,
     },
     depth = 100;
 
+var events =
+[
+    {
+      name : "Any",
+      shape : d3.symbolCircle,
+      color : "#1f77b4"
+    }
+
+,
+    {
+      name : "Duplication",
+      shape : d3.symbolSquare,
+      color : "#9467bd"
+    }
+,
+    {
+      name : "Speciation",
+      shape : d3.symbolCircle,
+      color : "#2ca02c"
+    }
+]
 
 //Instance element
 var svg = d3.select("body")
     .append("svg:svg")
+    .style("border","1px solid black")
     .attr("width", width)
     .attr("height", height)
     .attr("pointer-events", "all")
     .append("svg:g")
     .attr("transform","translate("+margin.left+","+margin.top+")");
 
+var glinks =
+   svg.append("svg:g");
 
+var gnodes =
+    svg.append("svg:g");
 
-
+var gconstraints =
+    svg.append("svg:g");
 
 //Data structures
 var treePatternRoot = {};
@@ -31,38 +58,135 @@ var treeComputer =
   d3.tree()
   .size([(width - margin.rigth -margin.left), (height - margin.top - margin.bottom)]);
 
+var symbol = d3.symbol();
+symbol.size(256);
+symbol.type(d3.symbolCross);
+
 
 function computeAndDraw() {
+
+  //Compute hierarchy and position
   var nodeRootHierarchy = d3.hierarchy(treePatternRoot);
   treeComputer(nodeRootHierarchy);
-  var nodes = nodeRootHierarchy.descendants();
 
-  nodesBind = svg.selectAll(".node")
-     .data(nodes);
+  //Bind data with element (nodes and links)
+  nodes = gnodes.selectAll(".node")
+    .data(nodeRootHierarchy.descendants());
 
-  nodesBind
-      .transition()
-      .attr("cx", function (d) {return d.x})
-      .attr("cy", function (d) {return d.depth * depth});
+  links = glinks.selectAll(".link")
+    .data(nodeRootHierarchy.links());
 
-  nodesBind
-     .enter()
-     .append("circle")
-     .classed("node",true)
-     .attr("cx", function (d) {return d.x})
-     .attr("cy", function (d) {return d.depth * depth})
-     .attr("r", 20)
-     .style("fill", "none")
-     .style("stroke", "purple")
-     .style("stroke-width", "5px")
-     .on("click",function (d) {
-       addChild(d.data,{});
-     });
+  constraints = gconstraints.selectAll(".constraint")
+    .data(nodeRootHierarchy.links());
+
+
+  //Draw links
+  links.exit().remove();
+
+  links
+    .attr("d",function (d) {
+      var path = d3.path();
+      path.moveTo(d.source.x,d.source.depth * depth);
+      path.lineTo(d.target.x,d.target.depth * depth);
+      path.closePath();
+      return path.toString();
+    });
+
+  links
+    .enter()
+    .append("path")
+    .classed("link",true)
+    .attr("d",function (d) {
+      var path = d3.path();
+      path.moveTo(d.source.x,d.source.depth * depth);
+      path.lineTo(d.target.x,d.target.depth * depth);
+      path.closePath();
+      return path.toString();
+    })
+    .style("fill", "none")
+    .style("stroke", "black")
+    .style("stroke-width", "2px");
+
+  //Draw links
+  constraints.exit().remove();
+
+
+
+  constraints
+    .attr("transform",function(d) {
+      var x = d.source.x + ((d.target.x - d.source.x) /2)
+      var y =(d.source.depth * depth) + (((d.target.depth * depth) - (d.source.depth * depth)) / 2 )
+      return "translate("+x+","+y+") rotate(45)";
+    });
+
+  constraints
+    .enter()
+    .append("g")
+    .classed("constraint",true)
+    .attr("transform",function(d) {
+      var x = d.source.x + ((d.target.x - d.source.x) /2)
+      var y =(d.source.depth * depth) + (((d.target.depth * depth) - (d.source.depth * depth)) / 2 )
+      return "translate("+x+","+y+") rotate(45)";
+    })
+    .append("path")
+    .style("fill","white")
+    .style("stroke", "black")
+    .style("stroke-width", "2px")
+    .attr("d",function (d) {
+      return symbol();
+    });
+
+  //Draw nodes
+  nodes.exit().remove();
+
+  nodes
+    .enter()
+    .append("circle")
+    .classed("node",true)
+    .attr("cx", function (d) {return d.x})
+    .attr("cy", function (d) {return d.depth * depth})
+    .attr("r", 10)
+    .style("stroke","white")
+    .style("stroke-width", "5px")
+    .style("fill", function (d) {
+      if(d.data.event){
+        return d.data.event.color;
+      }else {
+        return "black";
+      }
+    })
+    .on("click",function (d) {
+
+      if (window.event.ctrlKey) {
+        changeEvent(d.data)
+      }
+      else if (window.event.altKey) {
+        removeChildren(d.data)
+      }
+      else {
+        addChild(d.data,{});
+      }
+
+    });
+
+  nodes
+    .attr("cx", function (d) {return d.x})
+    .attr("cy", function (d) {return d.depth * depth})
+    .style("fill", function (d) {
+      if(d.data.event){
+        return d.data.event.color;
+      }else {
+        return "black";
+      }
+    });
+
+
 
 
 
 }
-  computeAndDraw();
+computeAndDraw();
+
 //Add child to treeNode
 function addChild (treeNode,childNode) {
   if(treeNode.children){
@@ -70,5 +194,33 @@ function addChild (treeNode,childNode) {
   }else {
     treeNode.children = [childNode];
   }
+  computeAndDraw();
+}
+
+//Add child to treeNode
+function removeChildren (treeNode) {
+  if(treeNode.children){
+    treeNode.children = []
+  }
+  computeAndDraw();
+}
+
+//Change node event
+function changeEvent (treeNode) {
+  if(!treeNode.eventId){
+    treeNode.eventId = 0;
+  }
+  treeNode.eventId = (treeNode.eventId + 1) % events.length ;
+  treeNode.event = events[treeNode.eventId];
+  computeAndDraw();
+}
+
+//Change node event
+function changeEventConstraint (treeNode) {
+  if(!treeNode.eventId){
+    treeNode.eventId = 0;
+  }
+  treeNode.eventId = (treeNode.eventId + 1) % events.length ;
+  treeNode.event = events[treeNode.eventId];
   computeAndDraw();
 }
